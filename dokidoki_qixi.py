d6489c0f553f097de699621193299e60eeafa14d
PhoenixWANG <1493736059@qq.com>
2021-08-13 20:35:39 +0800

    Update dokidoki_qixi.py

--- dokidoki_qixi.py
+++ dokidoki_qixi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul  2 16:57:21 2021
+Created on Fri Aug 13 16:57:21 2021
 
 @author: WANG Dapeng Phoenix
 """
@@ -9,9 +9,9 @@ import turtle as tl
 tl.pencolor('red')
 for i in range(20):
     tl.right(-45+i*15)
-    tl.circle(-50,180)#+i*30)  
+    tl.circle(-50,180)
     tl.fd(100)
-    tl.right(90)#+i*30)
+    tl.right(90)
     tl.fd(100)
-    tl.circle(-50,180)#+i*30)
+    tl.circle(-50,180)
 tl.done()
